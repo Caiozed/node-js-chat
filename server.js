16d140fb1b0d1debb41b87e30e7a444a9f16a3f5
Caio Neves <caio.nevesleite@gmail.com>
2017-12-02 20:12:10 +0000

    DB connection to heroku fix

--- server.js
+++ server.js
@@ -8,29 +8,48 @@ var mysql = require('mysql');
 var app = express();
 var bodyParser = require('body-parser');
 var con;
+var db_config = {};
 if(!process.env.ENVIROMENT){
-    con = mysql.createConnection({
+    db_config = {
       host: process.env.IP,
       user: "caiozed",
       password: "",
       database: "c9",
       multipleStatements: true
-    }); 
+    }; 
 }else{
-    con = mysql.createConnection({
+    db_config = {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       multipleStatements: true
-    }); 
+    }; 
 }
 
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
+function handleDisconnect() {
+  con = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  con.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  con.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 //
 // ## SimpleServer `SimpleServer(obj)`
